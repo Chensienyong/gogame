60f93d17d2961f005570f0b0c306e94766c2cd68
Chensienyong <ashxcellent_kingzr@yahoo.com>
2019-10-25 23:45:32 +0700

    initialize

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 600
+	screenHeight = 800
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("initializing SDL: ", err)
+		return
+	}
+
+	window, err := sdl.CreateWindow(
+		"Spaceshooter",
+		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight,
+		sdl.WINDOW_OPENGL)
+	if err != nil {
+		fmt.Println("initializing window: ", err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("initializing renderer: ", err)
+		return
+	}
+	defer renderer.Destroy()
+
+	img, err := sdl.LoadBMP("sprites/player.bmp")
+	if err != nil {
+		fmt.Println("loading palyer sprite: ", err)
+		return
+	}
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+
+		renderer.SetDrawColor(255, 255, 255, 255)
+		renderer.Clear()
+
+		renderer.Present()
+	}
+}
